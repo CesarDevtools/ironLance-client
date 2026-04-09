ff0bb17f36a7ffbb9f3ec706b51bb54cc2aa7dcb
Cesar <cessardevs@gmail.com>
2026-04-09 14:39:03 +0100

    feat: Implement IsAnon wrapper component

---
+++ src/components/IsAnon.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Navigate } from "react-router-dom";
+import { Center, Loader } from "@mantine/core";
+
+function IsAnon({ children }) {
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
+
+  // 1. Mientras comprobamos si hay alguien logueado
+  if (isLoading) {
+    return (
+      <Center style={{ height: "100vh" }}>
+        <Loader color="blue" size="xl" type="dots" />
+      </Center>
+    );
+  }
+
+  // 2. Si YA está logueado, no le dejamos estar aquí. 
+  // Lo mandamos a la Home o a su Dashboard principal.
+  if (isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  // 3. Si NO está logueado, le dejamos ver el Login/Signup
+  return children;
+}
+
+export default IsAnon;
