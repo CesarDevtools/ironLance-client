eaa9078c40c9b424654cbfc5f5a32a2230d742ef
Cesar <cessardevs@gmail.com>
2026-04-09 14:33:41 +0100

    feat: implement IsPrivate wrapper component

---
+++ src/components/IsPrivate.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Navigate } from "react-router-dom";
+import { Center, Loader } from "@mantine/core";
+
+function IsPrivate({ children, allowedRoles }) {
+  const { isLoggedIn, isLoading, user } = useContext(AuthContext);
+
+  // 1. Mientras validamos el token
+  if (isLoading) {
+    return (
+      <Center style={{ height: "100vh" }}>
+        <Loader color="blue" size="xl" type="dots" />
+      </Center>
+    );
+  }
+
+  // 2. Si no está logueado
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  // 3. Si está logueado pero la ruta requiere un rol específico que el usuario NO tiene
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" />;
+  }
+
+  // 4. Si todo esta logueado y tiene el rol necesario
+  return children;
+}
+
+export default IsPrivate;
