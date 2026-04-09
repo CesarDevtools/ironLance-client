b245c27191167af6791fc50300628980513a4a77
Cesar <cessardevs@gmail.com>
2026-04-09 11:48:37 +0100

    Code cleanup and setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { Notifications } from '@mantine/notifications';
+import { ModalsProvider } from '@mantine/modals';
+import '@mantine/notifications/styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProviderWrapper } from "./context/auth.context";
+
+
+const theme = createTheme({
+  primaryColor: "blue",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MantineProvider theme={theme}>
+      <Notifications />
+      <ModalsProvider>
+        <Router>
+          <AuthProviderWrapper>
+            <App />
+          </AuthProviderWrapper>
+        </Router>
+      </ModalsProvider>
+    </MantineProvider>
+  </React.StrictMode>,
+);
